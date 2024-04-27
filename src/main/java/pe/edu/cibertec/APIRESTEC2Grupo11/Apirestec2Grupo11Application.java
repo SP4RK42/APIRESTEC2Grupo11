70de56189a990d4ad500f58fe45103b77f045efb
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-04-26 23:41:21 -0500

    Inclusion del proyecto APIRESTEC2Grupo11

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Grupo11/Apirestec2Grupo11Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2Grupo11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestec2Grupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestec2Grupo11Application.class, args);
+	}
+
+}
