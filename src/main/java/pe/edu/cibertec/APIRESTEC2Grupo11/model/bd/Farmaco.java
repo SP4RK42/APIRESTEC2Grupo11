6b96c38933ce4011d3a54ae961c773ece6b1bd64
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-04-27 00:59:00 -0500

    Archivo Farmaco actualizado CesarCoaguila

--- src/main/java/pe/edu/cibertec/APIRESTEC2Grupo11/model/bd/Farmaco.java
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Grupo11/model/bd/Farmaco.java
@@ -1,4 +1,20 @@
 package pe.edu.cibertec.APIRESTEC2Grupo11.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Farmaco")
 public class Farmaco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer IdFarmaco;
+    private String NomFarmaco;
+    private String Composicion;
+    private Date FechaVencimiento;
 }
