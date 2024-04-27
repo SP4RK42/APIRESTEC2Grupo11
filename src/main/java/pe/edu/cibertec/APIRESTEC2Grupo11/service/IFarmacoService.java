b4319ca4c6a91ee47b14a78833f57efdca0f689c
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-04-27 01:23:54 -0500

    Añadido Service e IService de Farmaco

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Grupo11/service/IFarmacoService.java
@@ -0,0 +1,7 @@
+package pe.edu.cibertec.APIRESTEC2Grupo11.service;
+
+import pe.edu.cibertec.APIRESTEC2Grupo11.model.bd.Farmaco;
+
+public interface IFarmacoService {
+    Farmaco registrarFarmaco(Farmaco farmaco);
+}
